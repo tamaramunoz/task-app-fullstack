af6954d75b3cf9e70368b274ecba140b02c9309e
tamaramunoz <tami.skt@gmail.com>
2020-06-30 16:19:38 -0400

    methods get, post, put, delete in index.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('received')
+});
 
+app.post('/', (req, res) => {
+    res.send('saved')
+});
+
+app.put('/', (req, res) => {
+    res.send('updated')
+});
+
+app.delete('/', (req, res) => {
+    res.send('deleted')
+});
 
 module.exports = app;
