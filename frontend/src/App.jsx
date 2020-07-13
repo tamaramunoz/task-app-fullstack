ba5ec215466b2c798c02c8998979a355f0fc00ab
tamaramunoz <tami.skt@gmail.com>
2020-07-13 17:15:17 -0400

    installing bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Navigation from './components/Navigation'
 import CreateNote from './components/CreateNote'
 import CreateUser from './components/CreateUser'
