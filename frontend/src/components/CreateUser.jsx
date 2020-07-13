1bedea74ed2f456912b76cdf9e688b10363ece8b
tamaramunoz <tami.skt@gmail.com>
2020-07-13 18:11:15 -0400

    config server in backend with front

--- frontend/src/components/CreateUser.jsx
+++ frontend/src/components/CreateUser.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class CreateUser extends Component {
+
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/users')
+        console.log(res);
+    }
+
+
     render() {
         return (
             <div>
