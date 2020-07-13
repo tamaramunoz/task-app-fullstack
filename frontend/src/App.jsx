ab11b627122d72b4da271ebd292e555d7ad0642c
tamaramunoz <tami.skt@gmail.com>
2020-07-13 19:54:40 -0400

    create user component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,14 @@ import NotesList from './components/NotesList'
 const App = () => {
   return (
     <Router>
-        <Navigation />
+      <Navigation />
 
+      <div className="container p-4">
         <Route path="/" exact component={NotesList} />
         <Route path="/edit/:id" component={CreateNote} />
         <Route path="/create" component={CreateNote} />
         <Route path="/user" component={CreateUser} />
+      </div>
 
     </Router>
   )
