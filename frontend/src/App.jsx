7eb99863035373de2144c627535eb46b13d6a304
tamaramunoz <tami.skt@gmail.com>
2020-07-15 14:03:29 -0400

    background gradient and creating note form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 
 import Navigation from './components/Navigation'
 import CreateNote from './components/CreateNote'
--- frontend/src/components/CreateNote.jsx
+++ frontend/src/components/CreateNote.jsx
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateNote extends Component {
+
+    state = {
+        users: []
+    }
+
+    async componentDidMount () {
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({users: res.data})
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+
     render() {
         return (
-            <div>
-                CREATE NOTES
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h4>Create a Note</h4>
+
+                    <div className="form-group">
+                        <select 
+                            className="form-control"
+                            name="userSelected"
+                            
+                        >
+                            {
+                                this.state.users.map(user => 
+                                <option key={user._id}>
+                                    {user.username}
+                                </option>)
+                            }
+                        </select>
+                    </div>
+
+                    <form onSubmit={this.onSubmit}>
+                        <button type="submit" className="btn btn-primary">
+                            Save
+                        </button>
+                    </form>
+                </div>
             </div>
         );
     }
