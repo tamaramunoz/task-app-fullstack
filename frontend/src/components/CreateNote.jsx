e60d6ebaf017ed2627adac47c9b5ee891dce85da
tamaramunoz <tami.skt@gmail.com>
2020-07-15 16:45:02 -0400

    showing the tasks in the interface

--- frontend/src/components/CreateNote.jsx
+++ frontend/src/components/CreateNote.jsx
@@ -8,22 +8,36 @@ class CreateNote extends Component {
     state = {
         users: [],
         userSelected: '',
+        title: '',
+        content: '',
         date: new Date()
     }
 
     async componentDidMount () {
         const res = await axios.get('http://localhost:4000/api/users');
-        this.setState({users: res.data.map(user => user.username)})
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        })
         console.log(this.state.users)
     }
 
-    onSubmit = (e) => {
+    onSubmit = async(e) => {
         e.preventDefault();
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        }
+        await axios.post('http://localhost:4000/api/notes', newNote);
+        window.location.href = '/';
+        
     }
 
     onInputChange = (e) => {
         this.setState({
-            userSelected: e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
@@ -59,6 +73,7 @@ class CreateNote extends Component {
                             className="form-control"
                             placeholder="Title"
                             name="title"
+                            onChange={this.onInputChange}
                             required
                         />
                     </div>
@@ -68,6 +83,7 @@ class CreateNote extends Component {
                             name="content"
                             className="form-control"
                             placeholder="Content"
+                            onChange={this.onInputChange}
                             required
                         >
                         </textarea>
--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class NotesList extends Component {
+
+    state = {
+        notes: []
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/notes');
+        this.setState({
+            notes: res.data
+        })
+    }
+
     render() {
         return (
-            <div>
-                Note List
+            <div className="row">
+                {
+                    this.state.notes.map(note => (
+                        <div className="col-md-4 p-2" key={note._id}>
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5> {note.title} </h5>
+                                </div>
+                                <div className="card-body">
+                                    <p> {note.content} </p>
+                                    <p> {note.author} </p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         );
     }
